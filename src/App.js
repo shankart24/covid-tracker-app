68cd18ecd18c3ded3a8f33aa6ba38b640c6558de
Shan <itsshankart44@gmail.com>
2020-05-20 21:08:06 +0530

    added animation to the logo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SectionContent from "./Components/SectionContent";
 const App = () => {
 	return (
 		<main className="d-flex align-items-center">
-			<div className="container-fluid dashboard p-4">
+			<div className="container-fluid dashboard shadow-lg p-4">
 				<div className="row">
 					<div className="col-lg-3 col-md-4 col-sm-12 col-xs-12 header rounded">
 						<HeaderContent />
--- src/Components/SectionContent.js
+++ src/Components/SectionContent.js
@@ -1,13 +1,7 @@
 import React from "react";
 
 const SectionContent = () => {
-	return (
-		<div>
-			What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the
-			industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to
-			make a type specimen book it has?
-		</div>
-	);
+	return <div></div>;
 };
 
 export default SectionContent;
