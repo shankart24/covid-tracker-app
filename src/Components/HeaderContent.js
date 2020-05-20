51541522f583407824e2778c6d3973f85a698187
Shan <itsshankart44@gmail.com>
2020-05-20 20:04:13 +0530

    major layout changes

--- src/Components/HeaderContent.js
+++ src/Components/HeaderContent.js
@@ -3,8 +3,8 @@ import image1 from "../stock/image1.png";
 
 const HeaderContent = () => {
 	return (
-		<div className="row p-2 d-flex justify-content-center rounded">
-			<div className="col-lg-12 col-md-12 col-sm-8 header-content-one d-flex justify-content-center align-items-center flex-column">
+		<div className="row p-2 d-flex justify-content-center rounded header-content-row">
+			<div className="col-lg-12 col-md-12 col-sm-8 header-content-one d-flex justify-content-center align-items-center flex-column ">
 				<img src={image1} />
 				<h2>Covid19 Tracker</h2>
 			</div>
@@ -12,7 +12,7 @@ const HeaderContent = () => {
 				className="col-lg-12 col-md-12 col-sm-8 header-content-two d-flex 
 			justify-content-center align-items-center flex-column"
 			>
-				<h4>Keep track of the rising Covid19 cases in an area.</h4>
+				<h4>Keep track of the raising Covid19 cases in an area.</h4>
 				<input type="text" placeholder="Search" className="form-control" />
 			</div>
 		</div>
--- src/Components/SectionContent.js
+++ src/Components/SectionContent.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const SectionContent = () => {
-	return <div>Section</div>;
+	return (
+		<div>
+			What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the
+			industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to
+			make a type specimen book it has?
+		</div>
+	);
 };
 
 export default SectionContent;
